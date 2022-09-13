32bf0232e62220edad17b344d42570c72e942707
atulsingh0 <atul@circleci.com>
2022-09-13 17:33:29 -0400

    main | if else stmt

---
+++ freeCodeCamp/20-if-stmt.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if true {
+		fmt.Println("Hi, there")
+	}
+
+	marks := 77
+
+	if marks >= 75 {
+		fmt.Println("Passed with Honor")
+	} else if marks >= 60 && marks < 75 {
+		fmt.Println("Passed with First Division")
+	}
+
+	// logical operator
+	fmt.Println(marks > 75 && marks <= 90)
+	fmt.Println(marks > 75 || marks > 90)
+
+	fmt.Println(marks > 75 && returnTrue("dummy") && marks > 91)
+	fmt.Println("test 1 with returnTrue:", marks < 75 || marks > 91 || returnTrue("test 1"))
+
+	// Shortcircte in OR condition as first condition is true, Go did not evaluated further
+	fmt.Println("test 2 with returnTrue:", marks > 75 || returnTrue("test 2") || marks > 91)
+
+}
+
+func returnTrue(inp string) bool {
+	fmt.Println("Echo from returnTrue: call from", inp)
+	return true
+}
