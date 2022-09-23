dd1d6a79d4e34f7c51867d3a02b632e787d22de9
atulsingh0 <victimizeit@gmail.com>
2022-09-22 20:48:52 -0400

    main | slice

--- z_slice/main.go
+++ z_slice/main.go
@@ -45,5 +45,26 @@ func main() {
 	geta = append(geta, 3, 4, 5, 6)
 	fmt.Printf("\ngeta: %v", geta)
 	fmt.Printf("\ngeta length: %v", len(geta))
-	fmt.Printf("\ngeta capacity: %v", cap(geta))
+	fmt.Printf("\ngeta capacity: %v\n", cap(geta))
+
+	// Appending the slice
+	echo := []int{11, 22, 33}
+
+	// those 3 dot after the var will append the geta slice into echo slice
+	fmt.Println("Appended echo:", append(echo, geta...))
+
+	// another example
+	fmt.Println("Appending []bytes:", append([]byte("Hello"), "World!"...))
+	fmt.Println("Appending []bytes:", string(append([]byte("Hello"), "World!"...)))
+
+	// creating slice from range
+	names := [5]string{"albert", "query", "thomas", "arya", "alfred"}
+	fmt.Println("\narray of names:", names)
+
+	// slicing
+	set1 := names[:3]
+	set2 := names[2:]
+	set3 := names[2 : len(names)-1]
+	fmt.Println("Set1:", set1, "\nSet2:", set2, "\nSet3:", set3)
+
 }
