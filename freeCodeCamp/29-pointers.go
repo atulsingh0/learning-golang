6567eb83d9dc4d2e0e0268395f7f3ec96278023d
atulsingh0 <victimizeit@gmail.com>
2022-09-26 19:31:19 -0400

    added few more func

--- freeCodeCamp/29-pointers.go
+++ freeCodeCamp/29-pointers.go
@@ -19,4 +19,13 @@ func main() {
 	*y = 14
 	fmt.Println(x, *y)
 
+	// Adding 2 to var via pointer
+	fmt.Printf("Value of X: %v\n", x)
+	addTwo(&x)
+	fmt.Printf("Value of X after calling addTwo func: %v\n", x)
+}
+
+
+func addTwo( x *int) {
+	*x += 2
 }
