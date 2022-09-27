20a033254b2d0075a4410d31ad03254318828908
atulsingh0 <victimizeit@gmail.com>
2022-09-27 16:30:59 -0400

    main | func2

--- freeCodeCamp/30a-functions.go
+++ freeCodeCamp/30a-functions.go
@@ -15,6 +15,22 @@ func main() {
 	// calling addAll func
 	addAll(2, 3, 4, 5, 6, 7, 7)
 
+	fmt.Println("Cube of 3 is:", calcCube(3))
+
+	fmt.Println("Multiply 3 and 4: ", multiply(3, 4))
+
+	// calling divide func
+	if result, err := divide(3, 2); err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Result of 3 / 2 is :", result)
+	}
+
+	if result, err := divide(3, 0); err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Result of 3 / 0 is :", result)
+	}
 }
 
 // copy by value
@@ -32,10 +48,32 @@ func squareRef(n *float64) {
 }
 
 // variadic parameter
+// variadic paramter should be used only once and as a last parameter
 func addAll(data ...float64) {
 	var result float64
 	for _, d := range data {
 		result += d
 	}
-	fmt.Printf("Total of %v: %v ", data, result)
+	fmt.Printf("Total of %v: %v\n", data, result)
+}
+
+func calcCube(n float64) float64 {
+	return n * n * n
+}
+
+// named return variabled
+func multiply(m, n float64) (result float64) {
+	result = m * n
+	return
+}
+
+// multiple return value
+func divide(m, n float64) (float64, error) {
+
+	if n == 0 {
+		return 0.0, fmt.Errorf("Can not divide by 0.\n")
+	} else {
+		return m / n, nil
+	}
+
 }
