2dff66d46bef62f048a62e69932dab59b3095f39
atulsingh0 <3430610+atulsingh0@users.noreply.github.com>
2023-07-17 16:22:57 -0400

    main | added new

--- gin-rest/03-serve-multiple-files/files.go
+++ gin-rest/03-serve-multiple-files/files.go
@@ -14,7 +14,11 @@ func main() {
 		c.JSON(200, "Hello to Gin world!")
 	})
 
+	// serve the filesystem
 	router.StaticFS("/files", http.FileSystem(http.Dir("./data")))
+
+	// server the local filesystem
+	router.Static("/index", "./data")
 
 	router.Run(":3000")
 }
